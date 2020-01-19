04b67e27b6ebe65638d4faa1fbdbc8344a9e033d
dereklitao <dereklitao@outlook.com>
2020-01-19 23:57:02 +0800

    add transfer state

--- src/csro_devices/csro_motor_touch_3k2r.c
+++ src/csro_devices/csro_motor_touch_3k2r.c
@@ -2,36 +2,60 @@
 
 #ifdef MOTOR_TOUCH_3K2R
 
-#define THRESHOLD 15
+#define THRESHOLD 10
 
 #define TOUCH_01_NUM 0
 #define TOUCH_02_NUM 2
 #define TOUCH_03_NUM 3
 
-#define RELAY_UP_NUM GPIO_NUM_27
-#define RELAY_DOWN_NUM GPIO_NUM_14
+#define RELAY_OPEN_NUM GPIO_NUM_27
+#define RELAY_CLOSE_NUM GPIO_NUM_14
 
 #define LED_01_NUM GPIO_NUM_19
 #define LED_02_NUM GPIO_NUM_18
 #define LED_03_NUM GPIO_NUM_5
-#define GPIO_OUTPUT_PIN_SEL ((1ULL << RELAY_UP_NUM) | (1ULL << RELAY_DOWN_NUM) | (1ULL << LED_01_NUM) | (1ULL << LED_02_NUM) | (1ULL << LED_03_NUM))
+#define GPIO_OUTPUT_PIN_SEL ((1ULL << RELAY_OPEN_NUM) | (1ULL << RELAY_CLOSE_NUM) | (1ULL << LED_01_NUM) | (1ULL << LED_02_NUM) | (1ULL << LED_03_NUM))
+
+#define OPEN_POSITION 99
+#define CLOSE_POSITION 1
 
 typedef enum
 {
     STOP = 0,
-    UP = 1,
-    DOWN = 2,
+    OPEN = 1,
+    CLOSE = 2,
+    STOP_TO_OPEN = 3,
+    STOP_TO_CLOSE = 4,
 } motor_state;
 
 motor_state motor = STOP;
-int action = UP;
 
 static void csro_update_motor_touch_3k2r_state(void)
 {
+    if (mqttclient != NULL)
+    {
+        cJSON *state_json = cJSON_CreateObject();
+        cJSON *airsys_json;
+
+        cJSON_AddStringToObject(state_json, "time", sysinfo.time_str);
+        cJSON_AddNumberToObject(state_json, "run", (int)(sysinfo.time_now - sysinfo.time_start));
+        cJSON_AddItemToObject(state_json, "state", airsys_json = cJSON_CreateObject());
+        cJSON_AddStringToObject(airsys_json, "mode", work_mode[csro_airsys.mode]);
+        cJSON_AddNumberToObject(airsys_json, "temp", csro_airsys.temp);
+        cJSON_AddStringToObject(airsys_json, "fan", fan_mode[csro_airsys.fan]);
+        char *out = cJSON_PrintUnformatted(state_json);
+        strcpy(mqttinfo.content, out);
+        free(out);
+        cJSON_Delete(state_json);
+        sprintf(mqttinfo.pub_topic, "csro/%s/%s/state", sysinfo.mac_str, sysinfo.dev_type);
+        esp_mqtt_client_publish(mqttclient, mqttinfo.pub_topic, mqttinfo.content, 0, 0, 1);
+    }
 }
 
 static void motor_touch_3k2r_relay_led_task(void *args)
 {
+    static motor_state last_state = STOP;
+    static uint16_t count_100ms = 0;
     gpio_config_t io_conf;
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
     io_conf.mode = GPIO_MODE_OUTPUT;
@@ -42,86 +66,104 @@ static void motor_touch_3k2r_relay_led_task(void *args)
 
     while (true)
     {
-        if (motor == UP)
+        bool update = false;
+        if (last_state != motor)
         {
-            gpio_set_level(RELAY_UP_NUM, 1);
-            gpio_set_level(RELAY_DOWN_NUM, 0);
-            gpio_set_level(LED_01_NUM, 0);
-            gpio_set_level(LED_02_NUM, 1);
-            gpio_set_level(LED_03_NUM, 1);
+            last_state = motor;
+            update = true;
+
+            if (motor == STOP)
+            {
+                gpio_set_level(RELAY_OPEN_NUM, 0);
+                gpio_set_level(RELAY_CLOSE_NUM, 0);
+            }
+            else if (motor == OPEN)
+            {
+            }
         }
-        else if (motor == STOP)
-        {
-            gpio_set_level(RELAY_UP_NUM, 0);
-            gpio_set_level(RELAY_DOWN_NUM, 0);
-            gpio_set_level(LED_01_NUM, 1);
-            gpio_set_level(LED_02_NUM, 0);
-            gpio_set_level(LED_03_NUM, 1);
-        }
-        else if (motor == DOWN)
-        {
-            gpio_set_level(RELAY_UP_NUM, 0);
-            gpio_set_level(RELAY_DOWN_NUM, 1);
-            gpio_set_level(LED_01_NUM, 1);
-            gpio_set_level(LED_02_NUM, 1);
-            gpio_set_level(LED_03_NUM, 0);
-        }
-        vTaskDelay(25 / portTICK_PERIOD_MS);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
 }
 
 static void motor_touch_3k2r_key_task(void *args)
 {
-
     static uint16_t first_touch_value[4];
-    static uint16_t holdtime[4];
+    static uint16_t single_holdtime[3];
+    static uint16_t all_holdtime;
     touch_pad_init();
     touch_pad_set_voltage(TOUCH_HVOLT_2V7, TOUCH_LVOLT_0V5, TOUCH_HVOLT_ATTEN_1V);
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
     for (int i = 0; i < 4; i++)
     {
         touch_pad_config(i, 0);
-        vTaskDelay(100 / portTICK_PERIOD_MS);
         touch_pad_read(i, &first_touch_value[i]);
     }
 
     while (true)
     {
+        uint8_t touch_state[3];
         uint16_t touchvalue;
         for (uint8_t i = 0; i < 4; i++)
         {
             touch_pad_read(i, &touchvalue);
-            if (first_touch_value[i] > touchvalue && (first_touch_value[i] - touchvalue >= THRESHOLD))
+            if (i != 1)
             {
-                holdtime[i]++;
-                if (holdtime[i] == 4)
+                touch_state[i == 0 ? 0 : i == 2 ? 1 : 2] = (first_touch_value[i] > touchvalue && (first_touch_value[i] - touchvalue >= THRESHOLD));
+            }
+        }
+        if (touch_state[0] + touch_state[1] + touch_state[2] == 1)
+        {
+            all_holdtime = 0;
+            single_holdtime[0] = (single_holdtime[0] + 1) * touch_state[0];
+            single_holdtime[1] = (single_holdtime[1] + 1) * touch_state[1];
+            single_holdtime[2] = (single_holdtime[2] + 1) * touch_state[2];
+            if (single_holdtime[0] == 4)
+            {
+                if (motor == STOP || motor == STOP_TO_CLOSE)
                 {
-                    if (i == 0)
-                    {
-                        motor_state state = motor;
-                        motor = (state == STOP) ? UP : STOP;
-                    }
-                    else if (i == 2)
-                    {
-                        motor = STOP;
-                    }
-                    else if (i == 3)
-                    {
-                        motor_state state = motor;
-                        motor = (state == STOP) ? DOWN : STOP;
-                    }
+                    motor = OPEN;
+                }
+                else if (motor == CLOSE)
+                {
+                    motor = STOP_TO_OPEN;
                 }
             }
-            else
+            else if (single_holdtime[1] == 4)
             {
-                holdtime[i] = 0;
+                motor = STOP;
+            }
+            else if (single_holdtime[2] == 4)
+            {
+                if (motor == STOP || motor == STOP_TO_OPEN)
+                {
+                    motor = CLOSE;
+                }
+                else if (motor == OPEN)
+                {
+                    motor = STOP_TO_CLOSE;
+                }
             }
         }
-        if (holdtime[0] > 6000 && holdtime[2] > 6000 && holdtime[3] > 6000)
+        else if (touch_state[0] + touch_state[1] + touch_state[2] == 3)
         {
-            esp_restart();
+            all_holdtime++;
+            single_holdtime[0] = 0;
+            single_holdtime[1] = 0;
+            single_holdtime[2] = 0;
+            if (all_holdtime > 3000)
+            {
+                esp_restart();
+            }
         }
+        else
+        {
+            all_holdtime = 0;
+            single_holdtime[0] = 0;
+            single_holdtime[1] = 0;
+            single_holdtime[2] = 0;
+        }
+
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
     vTaskDelete(NULL);
